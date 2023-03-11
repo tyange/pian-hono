d89a1d43d2563e565918c21b1737335be8db966b
tyange <usun16@gmail.com>
2023-03-11 14:54:33 +0900

    DB 포트 변경

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ dotenv.config();
 
 const client = new Client({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 7153,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
 });
